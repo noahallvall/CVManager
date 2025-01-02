0b4ecd2029bb4c6e6b70190d4212878fe6e8dff8
noahallvall <146009156+noahallvall@users.noreply.github.com>
2025-01-02 10:55:45 +0100

    adda dbContext

---
+++ CVManager.DAL/Context/DBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVManager.DAL.Context
+{
+
+    public class BloggingContext : DbContext
+    {
+        //public DbSet<Blog> Blogs { get; set; }
+        //public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=blogsAndpostsDB;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Conventions.Remove(typeof(TableNameFromDbSetConvention));
+        }
+    }
+
+}

4a77797f0952cf9162610521dcd651443ba8bc7f
ErikR3 <153506923+ErikR3@users.noreply.github.com>
2025-01-06 18:25:13 +0100

    qwerqwer

---
+++ CVManager.WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using CVManager.DAL.Entities;
+using CVManager.WebApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+
+namespace CVManager.WebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userMngr, SignInManager<User>    signInMngr)
+        {
+            this.userManager = userMngr;
+            this.signInManager = signInMngr;
+        }
+
+        [HttpGet]
+
+        public IActionResult LogIn()
+        {
+            LoginViewModel loginViewModel = new LoginViewModel();
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ CVManager.WebApplication/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVManager.WebApplication.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Vänligen skriv ett användarnamn.")]
+        [StringLength(255)]
+
+        public string AnvandarNamn {  get; set; }
+
+        [Required(ErrorMessage = "Vänligen skriv ett lösenord.")]
+
+        public string Losenord { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}

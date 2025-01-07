5b14c444e1309ba58c50212105af7a34215ea2f5
ErikR3 <153506923+ErikR3@users.noreply.github.com>
2025-01-07 18:05:02 +0100

    qwer

--- CVManager.DAL/Entities/User.cs
+++ CVManager.DAL/Entities/User.cs
@@ -12,7 +12,13 @@ namespace CVManager.DAL.Entities
     {
         public int Id { get; set; }
 
-        public string FirstName { get; set; }    
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
 
         public bool IsPrivateProfile { get; set; }
 
---
+++ CVManager.DAL/Migrations/20250107170333_NewMigration.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVManager.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "CV");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "CV");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "CV");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "CV");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "CV");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "CV",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "CV",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "CV",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "CV",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "CV",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.UpdateData(
+                table: "CV",
+                keyColumn: "CVId",
+                keyValue: 1,
+                columns: new[] { "Address", "Email", "FirstName", "LastName", "Phone" },
+                values: new object[] { "Storgatan 3", "ClarkS@gmail.com", "Clark", "Smith", "7329329932" });
+        }
+    }
+}

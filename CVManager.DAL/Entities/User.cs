edea7f6b57bbad3f7a1d455aecafc17990f27f32
ErikR3 <153506923+ErikR3@users.noreply.github.com>
2025-01-08 14:59:42 +0100

    qwer

--- CVManager.DAL/Entities/User.cs
+++ CVManager.DAL/Entities/User.cs
@@ -10,27 +10,19 @@ namespace CVManager.DAL.Entities
 {
     public class User : IdentityUser
     {
-        public User() 
-        {
-            FirstName = "Pepega";
-            LastName = "Pepega";
-            Address = "Pepegatan 13";
-            Email = "Gooner@gmail.com";
-            Phone = "1234567890";
-        }
         public int Id { get; set; }
 
         public CV? CV { get; set; }
 
         public bool IsPrivateProfile {  get; set; }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
     }
 }

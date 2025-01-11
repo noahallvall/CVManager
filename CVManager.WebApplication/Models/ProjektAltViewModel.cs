bdbd7e3f40bf18628827c234ccaf8ff87cd4996b
ErikR3 <153506923+ErikR3@users.noreply.github.com>
2025-01-11 14:07:42 +0100

    Upp

---
+++ CVManager.WebApplication/Models/ProjektAltViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CVManager.DAL.Entities;
+
+namespace CVManager.WebApplication.Models
+{
+    public class ProjektAltViewModel
+    {
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
